cb92c929ad49d7b77d7a651eaeab236908e1cb86
Monika Bukowska <bukowskamonika9@gmail.com>
2021-12-14 17:25:07 +0100

    add favorites

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,5 +1,22 @@
+import { useContext } from 'react';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavouritesPage () {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. </p>
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />
+  }
+
   return(
-    <div>Favourites Page</div>
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
   )}
 export default FavouritesPage;
